eb6b12dd9b18d8c20901ceb97f153c21803b92be
Suranjana Sengupta <suranjana1128@gmail.com>
2014-10-28 23:37:15 +0800

    Added program to TOP for test

--- TOP.vhd
+++ TOP.vhd
@@ -242,3 +242,34 @@ end arch_TOP;
 --nor $t4, $t4, $zero
 --j loop
 --# n*3 (delay instructions) + 5 (non-delay instructions).
+
+
+----------------------------------------------------------------
+-- Test Program
+----------------------------------------------------------------
+
+--
+--ori $t1, 0x0001 # constant 1
+--#lui $t0, 0x1001 # DIP pointer, for MIPS simulation
+--lui $t0, 0x1003 # DIP pointer, for VHDL
+--lw  $t4, 0($t0)
+--lui $t0, 0x1002 # LED pointer, for VHDL
+--loop:
+--lui $t2, 0x0000
+--ori $t2, 0x0004 # delay counter (n). Change according to the clock
+--
+--ori $t5, 0x0004 # register to demonstrate AND
+--and $t2, $t2, $t5 # $2 value should stay the same after this line
+--or $t2, $t2, $t1  # $2 should be 0x0005 after this line
+--add $t2, $t2, $t1 # $2 should be 0x0006 after this line
+--
+--delay:
+--sub $t2, $t2, $t1
+--slt $t3, $t2, $t1
+--beq $t3, $zero, delay
+--sw  $t4, 0($t0)
+--nor $t4, $t4, $zero
+--j loop
+-- #n*3 (delay instructions) + 5 (non-delay instructions)
+ 
+
